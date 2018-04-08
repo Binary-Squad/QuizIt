4d2d78d7b4b66dc1badb29ee732bb083954c852c
Joe Faulstick <jfaulstick@gmail.com>
2018-04-08 00:10:32 -0700

    Added randomized clientAnswers to gameData for sending to the client

--- src/game.js
+++ src/game.js
@@ -23,12 +23,16 @@ function Game (questions, users, io){
         currentAnswer: undefined,
         // Game state variable for tracking Pre-Game, QuestionActive, Intermission, or GameEnd
         gameState: undefined,
+        // Client Answers for rendering on front-end
+        clientAnswers: {}
     };
 
     this.initializeGame = () => {
         // Start pre-game countdown to first question
         this.gameData.gameState = 'pre-game';
         this.gameData.currentQuestion = this.gameData.questions[this.gameData.questionNum];
+        this.gameData.currentAnswer = this.gameData.currentQuestion.correct_answer;
+        this.gameData.clientAnswers = this.randomizeAnswers();
         this.tickInterval();
     };
 
@@ -72,21 +76,63 @@ function Game (questions, users, io){
         // Function for setting the next current Question goes here.
         this.gameData.questionNum++;
         this.gameData.currentQuestion = questions[this.gameData.questionNum];
-        io.sockets.to('room').emit('msg', this.gameData.currentQuestion);
+        this.gameData.currentAnswer = this.gameData.currentQuestion.correct_answer;
+        this.gameData.clientAnswers = this.randomizeAnswers();
+        io.sockets.to('room').emit('msg', this.gameData);
         console.log("New question sent!");
         console.log(this.gameData.currentQuestion);
+        console.log(this.gameData.currentAnswer);
+        console.log(this.gameData.clientAnswers);
     }
 
     // Method for slightly modifying server's gameState for client view (Remove excess questions, etc.)
-    this.createClientState = () => {
-        let clientState = this.gameData;
-        clientState.questions = undefined;
+    // this.createClientState = () => {
+    //     let clientState = this.gameData;
+    //     console.log(clientState);
+    //     delete clientState.questions;
 
-        if (this.gameData.gameState == 'questionActive' || this.gameData.gameState == 'pre-game') {
-            clientState.currentAnswer = undefined;
+    //     if (this.gameData.gameState == 'questionActive' || this.gameData.gameState == 'pre-game') {
+    //         delete clientState.currentAnswer;
+    //         clientState.currentQuestion = this.createClientQuestion();
+    //     }
+    //     console.log(clientState);
+    //     return clientState;
+    // }
+
+    // Method or randomizing the answers and generating the client-facing question object.
+    this.randomizeAnswers = () => {
+        // Create temporary array to store our answers before shuffling
+        let answers = [];
+        // Push the correct answer
+        answers.push(this.gameData.currentQuestion.correct_answer);
+        // Concatenate the array of incorrect answers
+        answers = answers.concat(this.gameData.currentQuestion.incorrect_answers);
+        // Shuffle the array
+        this.shuffle(answers);
+        // Create object file to return
+        let answersObj = {};
+        // Map randomized array values to numeric key pairings
+        for (i = 0; i < answers.length; i++) {
+            answersObj[i] = answers[i];
         }
-        
-        return clientState;
+
+        return answersObj;
+    }
+
+    // Use Fisher-Yates Shuffle to randomize an array (to use with question answers)
+    this.shuffle = (array) => {
+        let currentIndex = array.length, temporaryValue, randomIndex;
+
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
     }
 
     this.tick = undefined;
@@ -102,7 +148,7 @@ function Game (questions, users, io){
         } else {
             console.log("Game tick interval. Game State: " + this.gameData.gameState + " with timer " + this.gameData.timer);
             this.gameData.timer--;
-            io.sockets.to('room').emit('roomState', this.createClientState());
+            io.sockets.to('room').emit('roomState', this.gameData);
         }
     }
 
