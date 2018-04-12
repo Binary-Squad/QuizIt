18b7166daa04285d8bcd9804bba020347aa1e8c5
Joe Faulstick <jfaulstick@gmail.com>
2018-04-12 00:15:09 -0700

    Heroku fix for API question total

--- utils/triviaAPI.js
+++ utils/triviaAPI.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const triviaAPI = (cb)=>{
-	axios.get('https://opentdb.com/api.php?amount=5&type=multiple')
+	axios.get('https://opentdb.com/api.php?amount=10&type=multiple')
 								//  ^^^^^^ should be 10 questions
 		.then(res=>{
 			cb(res);
