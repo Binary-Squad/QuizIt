3e553d76b169a905d1dd4b77a3181519bac85894
Joe Faulstick <jfaulstick@gmail.com>
2018-04-19 20:31:18 -0700

    Swapped out logo

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -5,16 +5,12 @@ const Logo =
     <div>
         <div className="">
             <div className="logo-span float-left">
-                <span className="q logo-main">Q</span>
-                <span className="u logo-main">u</span>
-                <span className="i logo-main">i</span>
-                <span className="z logo-main">z</span>
-                <span className="big-i logo-main">I</span>
-                <span className="t logo-main">t</span>
-                <span className="exc logo-main">!</span>
+                <img src="/assets/img/quizit-logo-white.png" className="quizit-logo"/>
             </div>
-            <span className="logo-text">Powered by </span>
-            <a href="https://opentdb.com/"><img src="/assets/img/open-trivia-logo.png" className="open-trivia-logo"/></a>
+            <div className="logo-span2 float-left">
+                <span className="logo-text">Powered by </span>
+                <a href="https://opentdb.com/"><img src="/assets/img/open-trivia-logo.png" className="open-trivia-logo"/></a>
+            </div>
         </div>
     </div>
 
